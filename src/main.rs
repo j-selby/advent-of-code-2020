f57bccba519a9ed46621105922f0e33753b51841
James <jselby@jselby.net>
2020-12-01 18:25:08 +1100

    Complete challenge

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let mut input = std::fs::read_to_string("input").expect("Failed to read input");
+    let numbers: Vec<i32> = input.split_whitespace().map(|num| num.parse().expect("Failed to parse number")).collect();
+
+    for num in &numbers {
+        for other_num in &numbers {
+            if num + other_num == 2020 {
+                println!("{} * {} = {}", num, other_num, num * other_num);
+            }
+        }
+    }
+
+    println!("input: {:?}", numbers);
 }
