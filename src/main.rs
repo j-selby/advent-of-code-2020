7e872b7e497bab00b5a26bf39079a7735157879a
James <jselby@jselby.net>
2020-12-02 17:11:14 +1100

    Complete day 2 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,35 @@
+#![feature(str_split_once)]
+
+use regex::Regex;
+
 fn main() {
     let mut input = std::fs::read_to_string("input").expect("Failed to read input");
-    let numbers: Vec<i32> = input.split_whitespace().map(|num| num.parse().expect("Failed to parse number")).collect();
 
-    for num in &numbers {
-        for other_num in &numbers {
-            for third_num in &numbers {
-                if num + other_num + third_num == 2020 {
-                    println!("{} * {} * {} = {}", num, other_num, third_num, num * other_num * third_num);
-                }
-            }
-        }
-    }
+    let re = Regex::new(r"^(?P<from>[0-9]+)-(?P<to>[0-9]+) (?P<policy>\w): (?P<password>.*)$").unwrap();
 
-    println!("input: {:?}", numbers);
+    let result = input.lines()
+        .map(|line| {
+            println!("{:?}", line);
+            // Parse line
+            let captures = re.captures(line).expect("Failed to parse password");
+            let start : i32 = captures.name("from").expect("No from range").as_str().parse()
+                .expect("Failed to parse starting number");
+            let end : i32 = captures.name("to").expect("No to range").as_str().parse()
+                .expect("Failed to parse ending number");
+            let policy = captures.name("policy").expect("No match letter").as_str();
+            let password = captures.name("password").expect("No password").as_str();
+
+            // Find policy matches
+            println!("{} - {} ({:?}): {:?}", start, end, policy, password);
+
+            let count = password.matches(policy).count() as i32;
+            let metric = if count >= start && count <= end {true} else {false};
+            println!("Count {}: {}", count, metric);
+
+            metric
+        })
+        .filter(|x| *x)
+        .count();
+
+    println!("input: {:?}", result);
 }
