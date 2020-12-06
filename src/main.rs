99536554fef7104713a4e57f0ef0aadcd4cd4964
James <jselby@jselby.net>
2020-12-06 17:26:47 +1100

    Complete day 6 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use itertools::Itertools;
+#![feature(iterator_fold_self)]
+
+use std::collections::BTreeSet;
 
 fn main() {
     let input = std::fs::read_to_string("input").expect("Failed to read input");
 
     let result : usize = input
         .split("\n\n")
-        .map(|x| x.chars().filter(|x| x.is_alphabetic()).unique().count())
+        .map(|x| {
+            x.split_whitespace().map(|passenger| passenger.chars().collect::<BTreeSet<char>>())
+                .fold_first(|acc, a| acc.intersection(&a).map(|x| *x).collect())
+                .expect("Failed to fold elements")
+                .len()
+        })
         .sum();
 
     println!("{:?}", result);
