709e9ab8ad6aa8c03edacfd1c3857f956e7b6478
James <jselby@jselby.net>
2020-12-06 17:18:02 +1100

    Complete day 6 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,12 @@
-#![feature(str_split_once)]
-
-#[derive(Debug)]
-struct Seat {
-    row: u32,
-    column: u32
-}
-
-impl Seat {
-    fn get_seat_id(&self) -> u32 {
-        self.row * 8 + self.column
-    }
-}
+use itertools::Itertools;
 
 fn main() {
     let input = std::fs::read_to_string("input").expect("Failed to read input");
 
-    let mut result = input.lines()
-        .map(|line| {
-            let (mut row_min, mut row_max) = (0, 127);
-            let (mut column_min, mut column_max) = (0, 7);
+    let result : usize = input
+        .split("\n\n")
+        .map(|x| x.chars().filter(|x| x.is_alphabetic()).unique().count())
+        .sum();
 
-            let row_div = line.chars().take(7);
-            let col_div = line.chars().skip(7).take(3);
-
-            for row_option in row_div {
-                let diff = (row_max - row_min) + 1;
-                match row_option {
-                    'F' => {
-                        row_max -= diff / 2;
-                    }
-                    'B' => {
-                        row_min += diff / 2;
-                    }
-                    x => panic!("Bad row char: {:?}", x)
-                }
-            }
-
-            for col_option in col_div {
-                let diff = (column_max - column_min) + 1;
-                match col_option {
-                    'L' => {
-                        column_max -= diff / 2;
-                    }
-                    'R' => {
-                        column_min += diff / 2;
-                    }
-                    x => panic!("Bad col char: {:?}", x)
-                }
-            }
-
-
-            let seat =
-                Seat {
-                    row: row_min,
-                    column: column_min
-                };
-
-            println!("{:?} = {:?}", line, seat);
-
-            seat
-        }).map(|x| x.get_seat_id()).collect::<Vec<_>>();
-
-    result.sort();
-
-    // Find a "hole" in result
-    for i in 1 .. result.len() - 1 {
-        if result[i - 1] + 1 != result[i] {
-            println!("Found a gap between {} and {}", result[i - 1], result[i])
-        }
-    }
+    println!("{:?}", result);
 }
