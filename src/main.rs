87a20b5ce2f914f2dff24cb153bf9062e589f778
James <jselby@jselby.net>
2020-12-12 16:15:45 +1100

    Complete day 10 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
+use itertools::Itertools;
+
 fn main() {
     let input = std::fs::read_to_string("input").expect("Failed to read input");
 
-    let input = input
+    let mut input = input
         .lines()
         .map(|line| {
             line.parse::<i64>()
@@ -9,56 +11,34 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    let mut bad_num = None;
+    input.push(0);
+    input.push(input.iter().max().expect("Data set was empty!") + 3);
 
-    for (index, num) in input.iter().enumerate().skip(25) {
-        let preamble = &input[index - 25..index];
-        // Calculate if any preamble pair matches
-        let mut found_match = false;
+    input.sort();
 
-        'main_loop: for num_a in preamble {
-            for num_b in preamble {
-                if num_a + num_b == *num {
-                    found_match = true;
-                    break 'main_loop;
-                }
-            }
-        }
+    // Calculate the deltas
+    let differences = input
+        .iter()
+        //.skip(1)
+        .zip(input.iter().skip(1))
+        .map(|(left, right)| right - left)
+        .collect::<Vec<_>>();
 
-        if !found_match {
-            println!("Bad match: {:?}", num);
-            bad_num = Some((index, *num));
-            break;
-        }
-    }
+    let difference_options = differences
+        .iter()
+        .unique()
+        .map(|unique_option| {
+            (
+                unique_option,
+                differences.iter().filter(|x| *x == unique_option).count(),
+            )
+        })
+        .collect::<Vec<_>>();
 
-    let (bad_index, bad_num) = bad_num.expect("No match in the data set");
+    // Multiply our options
+    let multiplied = difference_options
+        .iter()
+        .fold(1, |acc, (_, count)| acc * *count);
 
-    // Find the weakness
-    for starting_range in 0..bad_index {
-        // See if we get a sum which matches
-        let mut sum = 0;
-        for ending_range in starting_range..bad_index {
-            sum += input[ending_range];
-
-            if sum == bad_num {
-                println!(
-                    "Range: {} ({}) .. {} ({})",
-                    starting_range, input[starting_range], ending_range, input[ending_range]
-                );
-
-                // Find the smallest number in this range
-                let min = input[starting_range..=ending_range]
-                    .iter()
-                    .min()
-                    .expect("No numbers in range");
-                let max = input[starting_range..=ending_range]
-                    .iter()
-                    .max()
-                    .expect("No numbers in range");
-
-                println!("Min/max: {} + {} = {}", min, max, min + max);
-            }
-        }
-    }
+    println!("{:?}", multiplied);
 }
