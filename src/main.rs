547ebdc6b4ff50628582a2a8bc7fc372d13c0b7b
James <jselby@jselby.net>
2020-12-12 17:10:48 +1100

    Complete day 12 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,126 +1,93 @@
-#[derive(Clone, Ord, PartialOrd, Eq, PartialEq)]
-enum SeatType {
-    Empty,
-    Occupied,
-    Floor,
+/// Converts the ships heading into a distance "delta".
+fn heading_to_delta(heading: i32) -> (i32, i32) {
+    match heading {
+        0 => (0, -1),
+        90 => (1, 0),
+        180 => (0, 1),
+        270 => (-1, 0),
+        x => panic!("Bad heading: {}", x)
+    }
+}
+
+/// Converts a heading into an absolute figure.
+///
+/// e.g.
+/// 0 degrees = 0 degrees
+/// 90 degrees = 90 degrees
+/// 361 degrees = 1 degree
+/// -1 degrees = 359 degrees
+fn absolute_heading(mut heading: i32) -> i32 {
+    while heading < 0 {
+        heading += 360;
+    }
+
+    heading % 360
+}
+
+#[derive(Debug)]
+enum ActionType {
+    North,
+    South,
+    East,
+    West,
+    Left,
+    Right,
+    Forward
+}
+
+#[derive(Debug)]
+struct Action {
+    kind: ActionType,
+    value: i32
 }
 
 fn main() {
-    let input = std::fs::read_to_string("input").expect("Failed to read input");
-
-    let mut input = input
+    let input = std::fs::read_to_string("input").expect("Failed to read input")
         .lines()
         .filter(|line| !line.trim().is_empty())
         .map(|line| {
-            line.chars()
-                .map(|x| match x {
-                    'L' => SeatType::Empty,
-                    '#' => SeatType::Occupied,
-                    '.' => SeatType::Floor,
-                    x => panic!("Invalid char: {:?}", x),
-                })
-                .collect::<Vec<_>>()
+            let (action, distance) = line.split_at(1);
+
+            let kind = match action {
+                "N" => ActionType::North,
+                "S" => ActionType::South,
+                "E" => ActionType::East,
+                "W" => ActionType::West,
+                "L" => ActionType::Left,
+                "R" => ActionType::Right,
+                "F" => ActionType::Forward,
+                x => panic!("Invalid action: {:?}", x)
+            };
+
+            Action {
+                kind,
+                value: distance.parse().expect("Failed to parse distance")
+            }
         })
         .collect::<Vec<_>>();
 
-    let direction_options = [
-        (-1, -1),
-        (0, -1),
-        (1, -1),
-        (-1, 0),
-        (1, 0),
-        (-1, 1),
-        (0, 1),
-        (1, 1)
-    ];
+    let mut heading = 90;
+    let (mut x, mut y) = (0i32, 0i32);
 
-    // Run iterations
-    let mut count = 0;
-    loop {
-        // Take a copy for stagnation checks
-        let last_input = input.clone();
+    for action in input {
+        println!("Executing action: {:?}", action);
 
-        for (y, row) in last_input.iter().enumerate() {
-            for (x, seat) in row.iter().enumerate() {
-                // Get adjacent seats
-                let mut adjacent_seats = 0;
-
-
-                for (direction_x, direction_y) in &direction_options {
-                    let mut check_x = x as i32;
-                    let mut check_y = y as i32;
-
-                    // Wait until we find a seat we can see
-                    'main_loop:
-                    loop {
-                        check_x += direction_x;
-                        check_y += direction_y;
-
-                        if check_y >= 0
-                            && check_x >= 0
-                            && check_y < last_input.len() as i32
-                            && check_x < row.len() as i32
-                            //&& !(check_x == x_abs && check_y == y_abs)
-                        {
-                            match last_input[check_y as usize][check_x as usize] {
-                                SeatType::Empty => break 'main_loop,
-                                SeatType::Occupied => {
-                                    adjacent_seats += 1;
-                                    break 'main_loop;
-                                }
-                                SeatType::Floor => continue 'main_loop,
-                            }
-                        } else {
-                            break 'main_loop;
-                        }
-                    }
-                }
-
-                match seat {
-                    SeatType::Empty => {
-                        if adjacent_seats == 0 {
-                            input[y][x] = SeatType::Occupied;
-                        }
-                    }
-                    SeatType::Occupied => {
-                        if adjacent_seats >= 5 {
-                            input[y][x] = SeatType::Empty;
-                        }
-                    }
-                    SeatType::Floor => {
-                        // No-op
-                    }
-                }
+        match action.kind {
+            ActionType::North => y -= action.value,
+            ActionType::South => y += action.value,
+            ActionType::East => x += action.value,
+            ActionType::West => x -= action.value,
+            ActionType::Left => heading = absolute_heading(heading - action.value),
+            ActionType::Right => heading = absolute_heading(heading + action.value),
+            ActionType::Forward => {
+                let (x_delta, y_delta) = heading_to_delta(heading);
+                x += x_delta * action.value;
+                y += y_delta * action.value;
             }
         }
 
-        count += 1;
-
-        println!("Completed iter: {}", count);
-
-        // Print state
-        for row in input.iter() {
-            for seat in row.iter() {
-                let char = match seat {
-                    SeatType::Empty => 'L',
-                    SeatType::Occupied => '#',
-                    SeatType::Floor => '.'
-                };
-                print!("{}", char);
-            }
-            println!();
-        }
-
-        if input == last_input {
-            println!("Completed after {} counts", count);
-            break;
-        }
+        println!("New location: {} x {}", x, y);
     }
 
-    let occupied_seats : usize = input
-        .iter()
-        .map(|x| x.iter().filter(|x| **x == SeatType::Occupied).count())
-        .sum();
-
-    println!("Seats occupied: {}", occupied_seats);
+    println!("Distance: {}", x.abs() + y.abs());
 }
