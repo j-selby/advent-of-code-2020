5a01a1d3576ad808150b156ce9a4b09c895c2037
James <jselby@jselby.net>
2020-12-18 20:51:05 +1100

    Complete day 16 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,102 @@
-use std::collections::HashMap;
+#![feature(str_split_once)]
+
+#[derive(Debug)]
+struct Rule<'a> {
+    name: &'a str,
+    ranges: Vec<(i64, i64)>,
+}
+
+#[derive(Debug)]
+struct Ticket {
+    numbers: Vec<i64>,
+}
+
+fn parse_tickets(input: &str) -> Vec<Ticket> {
+    input
+        .lines()
+        .skip(1)
+        .map(|line| Ticket {
+            numbers: line
+                .split(",")
+                .map(|line_value| {
+                    line_value
+                        .parse()
+                        .expect("Failed to parse ticket line value")
+                })
+                .collect(),
+        })
+        .collect()
+}
+
+/// Evaluates if a value matches a rule
+fn evaluate_rule(value: i64, rule: &Rule) -> bool {
+    for (low, high) in &rule.ranges {
+        if value >= *low && value <= *high {
+            return true;
+        }
+    }
+
+    false
+}
+
+/// Validates that a ticket can have any possible match for any rule
+fn ticket_error_rate(ticket: &Ticket, rules: &[Rule]) -> i64 {
+    // Check if any numbers *don't* have a matching rule and invert
+    ticket
+        .numbers
+        .iter()
+        .filter(|num| {
+            rules
+                .iter()
+                .filter(|rule| evaluate_rule(**num, rule))
+                .count()
+                == 0
+        })
+        .sum()
+}
 
 fn main() {
-    let input = "1,0,15,2,10,13"
-        .split(",")
-        .map(|x| x.parse().expect("Failed to parse"))
-        .collect::<Vec<i64>>();
+    let input = std::fs::read_to_string("input").expect("Failed to read input");
 
-    // (number, turn)
-    let mut last_spoken_turns = HashMap::new();
+    // 3 sections: rules, your ticket, nearby tickets. split this:
+    let mut input_splits = input.split("\n\n");
 
-    let mut last_spoken = 0;
-    let mut last_spoken_turn = None;
+    let rules = input_splits.next().expect("Failed to find rules section");
+    let your_ticket = input_splits.next().expect("Failed to find your ticket");
+    let nearby_tickets = input_splits.next().expect("Failed to find nearby tickets");
 
-    let mut turn = 1i64;
+    // First, parse the rules
+    let rules = rules
+        .lines()
+        .map(|line| {
+            let (name, matches) = line.split_once(": ").expect("Failed to find delimiter");
+            let ranges = matches
+                .split(" or ")
+                .map(|range| {
+                    let (low, high) = range
+                        .split_once("-")
+                        .expect("Failed to find range delimiter");
+                    (
+                        low.parse().expect("Failed to parse low"),
+                        high.parse().expect("Failed to parse high"),
+                    )
+                })
+                .collect();
 
-    for num in input {
-        last_spoken_turns.insert(num, turn);
+            Rule { name, ranges }
+        })
+        .collect::<Vec<_>>();
 
-        // Iterate
-        last_spoken = num;
-        turn += 1;
-    }
+    // and the tickets:
+    let your_ticket = parse_tickets(your_ticket)
+        .pop()
+        .expect("Failed to find ticket");
+    let nearby_tickets = parse_tickets(nearby_tickets);
 
-    println!("Starting turn: {}", turn);
+    let bad_tickets : i64 = nearby_tickets
+        .iter()
+        .map(|ticket| ticket_error_rate(ticket, &rules))
+        .sum();
 
-    while turn <= 30000000 {
-        let output_num = match last_spoken_turn {
-            Some(last_turn) => turn - last_turn - 1,
-            None => 0
-        };
-
-        //println!("Turn {}: last {}, current {}, was first: {:?}", turn, last_spoken, output_num, last_spoken_turn);
-
-        last_spoken_turn = last_spoken_turns.insert(output_num, turn);
-
-        // Iterate
-        last_spoken = output_num;
-        turn += 1;
-    }
-
-    println!("{}", last_spoken);
+    println!("{:#?}", bad_tickets);
 }
