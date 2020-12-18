7784a81a1ba72e0a9148ebf4f122e60ebf803822
James <jselby@jselby.net>
2020-12-18 18:37:24 +1100

    Complete day 13 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,38 @@
-/// Converts a heading into an absolute figure.
-///
-/// e.g.
-/// 0 degrees = 0 degrees
-/// 90 degrees = 90 degrees
-/// 361 degrees = 1 degree
-/// -1 degrees = 359 degrees
-fn absolute_heading(mut heading: i32) -> i32 {
-    while heading < 0 {
-        heading += 360;
-    }
-
-    heading % 360
-}
-
-#[derive(Debug, Ord, PartialOrd, Eq, PartialEq)]
-enum ActionType {
-    North,
-    South,
-    East,
-    West,
-    Left,
-    Right,
-    Forward
-}
-
-#[derive(Debug)]
-struct Action {
-    kind: ActionType,
-    value: i32
-}
-
 fn main() {
-    let input = std::fs::read_to_string("input").expect("Failed to read input")
-        .lines()
-        .filter(|line| !line.trim().is_empty())
-        .map(|line| {
-            let (action, distance) = line.split_at(1);
+    let input = std::fs::read_to_string("input")
+        .expect("Failed to read input");
 
-            let kind = match action {
-                "N" => ActionType::North,
-                "S" => ActionType::South,
-                "E" => ActionType::East,
-                "W" => ActionType::West,
-                "L" => ActionType::Left,
-                "R" => ActionType::Right,
-                "F" => ActionType::Forward,
-                x => panic!("Invalid action: {:?}", x)
-            };
+    let mut input = input.lines();
 
-            Action {
-                kind,
-                value: distance.parse().expect("Failed to parse distance")
-            }
-        })
-        .collect::<Vec<_>>();
+    let earliest_time = input
+        .next()
+        .expect("Failed to find earliest time")
+        .parse::<i32>()
+        .expect("Failed to parse earliest time");
 
-    let (mut x, mut y) = (0i32, 0i32);
-    let (mut way_x, mut way_y) = (10i32, -1i32);
+    let bus_times: Vec<i32> = input
+        .next()
+        .expect("Failed to find bus IDs")
+        .split(",")
+        .filter(|x| x != &"x")
+        .map(|x| x.parse().expect("Failed to parse bus ID"))
+        .collect();
 
-    for action in input {
-        println!("Executing action: {:?}", action);
+    let mut current_time = earliest_time;
+    let matching_id;
 
-        match action.kind {
-            ActionType::North => way_y -= action.value,
-            ActionType::South => way_y += action.value,
-            ActionType::East => way_x += action.value,
-            ActionType::West => way_x -= action.value,
-            ActionType::Left | ActionType::Right => {
-                let mut degrees = if action.kind == ActionType::Left {
-                    absolute_heading(-action.value)
-                } else {
-                    action.value
-                };
-
-                while degrees > 0 {
-                    let old_x = way_x;
-                    let old_y = way_y;
-
-                    way_y = old_x;
-                    way_x = -old_y;
-
-                    degrees -= 90;
-                }
-            },
-            ActionType::Forward => {
-                x += way_x * action.value;
-                y += way_y * action.value;
+    loop {
+        match bus_times.iter().filter(|x| current_time % **x == 0).next() {
+            None => {},
+            Some(match_id) => {
+                matching_id = *match_id;
+                break
             }
         }
 
-        println!("New location: {} x {}", x, y);
-        println!("Way: {} x {}", way_x, way_y);
+        current_time += 1;
     }
 
-    println!("Distance: {}", x.abs() + y.abs());
+    println!("Matching ID: {} @ {}", matching_id, current_time);
+    println!("Wait time: {}", matching_id * (current_time - earliest_time));
 }
