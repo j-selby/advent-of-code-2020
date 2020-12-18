5ae56606c47d0b678b4f1e1737566a6e7d7740a5
James <jselby@jselby.net>
2020-12-18 19:22:35 +1100

    Complete day 13 part 2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+fn modulo(mut x : i64, m : i64) -> i64 {
+    while x < 0 {
+        x += m;
+    }
+
+    x % m
+}
+
 fn main() {
     let input = std::fs::read_to_string("input")
         .expect("Failed to read input");
@@ -7,32 +15,38 @@ fn main() {
     let earliest_time = input
         .next()
         .expect("Failed to find earliest time")
-        .parse::<i32>()
+        .parse::<u64>()
         .expect("Failed to parse earliest time");
 
-    let bus_times: Vec<i32> = input
+    let bus_times: Vec<Option<u64>> = input
         .next()
         .expect("Failed to find bus IDs")
         .split(",")
-        .filter(|x| x != &"x")
-        .map(|x| x.parse().expect("Failed to parse bus ID"))
+        .map(|x| {
+            if x == "x" {
+                None
+            } else {
+                Some(x.parse().expect("Failed to parse bus ID"))
+            }
+        })
         .collect();
 
-    let mut current_time = earliest_time;
-    let matching_id;
+    let bus_times_n_offset : Vec<(usize, u64)> = bus_times.iter().enumerate()
+        .filter(|(_x, delta)| delta.is_some())
+        .map(|(x, delta)| (x, delta.unwrap()))
+        .collect();
 
-    loop {
-        match bus_times.iter().filter(|x| current_time % **x == 0).next() {
-            None => {},
-            Some(match_id) => {
-                matching_id = *match_id;
-                break
-            }
-        }
+    let u : Vec<i64> = bus_times_n_offset.iter()
+        .map(|(x, delta)| modulo(-(*x as i64), *delta as i64))
+        .collect();
 
-        current_time += 1;
-    }
+    let s : Vec<i64> = bus_times_n_offset.iter()
+        .map(|(_x, delta)| *delta as i64)
+        .collect();
 
-    println!("Matching ID: {} @ {}", matching_id, current_time);
-    println!("Wait time: {}", matching_id * (current_time - earliest_time));
+    // Fuck number theory:
+    let solution = ring_algorithm::chinese_remainder_theorem(&u, &s);
+
+    // 604510652639806
+    println!("{:?}", solution);
 }
